35cc7113f39e6446bf6380d5f5f392e9f5518137
Iurii Ernich <iurii.ernich@gmail.com>
2025-01-07 21:44:54 -0400

    Add Diplomado and Module Name

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,8 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h2>Diplomado Full Stack</h2>
+      <h3>Desarrollo con React</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
