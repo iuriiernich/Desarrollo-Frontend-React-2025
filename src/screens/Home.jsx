3b6175f181f2f554d344d78fc50623f3c3e10770
Iurii Ernich <iurii.ernich@gmail.com>
2025-01-08 21:46:20 -0400

    Manage Props

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,14 +1,18 @@
-import Logo from '../Components/Logo.jsx'; 
-import Body from '../Components/Body.jsx';
-import Footer from '../Components/Footer.jsx';
-import GitHub from '../Components/GitHub.jsx';
 
+import Body from '../Components/Body.jsx';
+import OpenLink from '../Components/Core/OpenLink.jsx';
+import Footer from '../Components/Footer.jsx';
+
+import viteLogo from "../assets/vite.svg";
+import reactLogo from "../assets/react.svg";
+import githubLogo from "../assets/github.svg";
 
 const Home = () => {
     return (
         <div>
-            <Logo />
-            <GitHub />
+            <OpenLink redirectURL={"https://vite.dev"} logoImage={viteLogo} altValue= "Vite"/>
+            <OpenLink redirectURL={"https://react.dev"} logoImage={reactLogo} altValue= "React"/>
+            <OpenLink redirectURL={"https://github.com/iuriiernich/Desarrollo-Frontend-React-2025"} logoImage={githubLogo} altValue= "GitHub"/>
             <Body />  
             <Footer />  
         </div>
