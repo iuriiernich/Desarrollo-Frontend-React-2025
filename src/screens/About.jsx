ba6de4924f4ee07ed769af785178549a8be3bb11
Iurii Ernich <iurii.ernich@gmail.com>
2025-01-14 22:00:52 -0400

    add use state

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -1,9 +1,19 @@
+import { useState } from 'react';
 const About = () => {
+    // los hooks en react son funciones ya definidas
+    // los hooks siempre tienen el prefijo use
+    // hook useState, useEffect, useContext, useRef, useTransition
+    // es posible crear nuestros propios hooks: useService, useImage, useForm, useCounter
+
+    // el hook useState me permite manejar variables de esado a nivel de componente
+    const [ nameComponent, setNameComponent ] = useState ('About');
+    
+
     return (
         <>
-            <p>Component About</p>
+            <p>Component name: {nameComponent}</p>
+            <button onClick={() => setNameComponent('Curso de React!!!')}>Change Name</button>
         </>
     );
-};
-
+}
 export default About;
